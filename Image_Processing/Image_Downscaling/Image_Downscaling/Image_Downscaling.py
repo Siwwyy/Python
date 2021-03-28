88b8ad6ca4be94a587c25c844e2655b7c6b63393
Andrysiak <DAndrysiak@luxoft.com>
2021-03-28 14:52:38 +0200

    final version of image downsampling with commentary

--- Image_Processing/Image_Downscaling/Image_Downscaling/Image_Downscaling.py
+++ Image_Processing/Image_Downscaling/Image_Downscaling/Image_Downscaling.py
@@ -1,120 +1,4 @@
 
-
-# import cv2
-# from skimage import data, color
-# from skimage.transform import rescale, resize, downscale_local_mean
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import os
-
-# from PIL import Image
-
-
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# PHOTO_DIR = "4K_1.jpg"
-
-
-#img_array = cv2.imread(PATH,cv2.IMREAD_COLOR)
-
-#print(img_array.shape)
-#image_downscaled = downscale_local_mean(img_array, (4,4,1))
-
-#fig, axes = plt.subplots(nrows=2, ncols=1)
-
-#ax = axes.ravel()
-
-#ax[0].imshow(img_array)
-#ax[0].set_title("Original image")
-
-#ax[1].imshow(image_downscaled / 255.0)
-#ax[1].set_title("Downscaled image (no aliasing)")
-
-#plt.tight_layout()
-#plt.show()
-
-
-#plt.figure(figsize=(1000,1000))
-#plt.imshow(img_array[...,::-1])
-##plt.tight_layout()
-#plt.show()
-
-####################################################################
-####################################################################
-####################################################################
-####################################################################
-
-
-
-#from PIL import Image
-#import numpy as np
-#import skimage
-
-
-#img = Image.open(PATH)
-#img.load()
-
-#img_array = np.asarray(img, dtype='int32')
-
-#red1 = np.copy(img_array)
-#red1[:,:,0] = 255 # this is on (top row below)
-#plt.imshow(red1)
-#plt.show()
-
-#blue0 = np.copy(img_array)
-#blue0[:,:,2] = 0 # this is off (bottom row below)
-#plt.imshow(blue0)
-
-#plt.show()
-
-#downsample = 8
-## first, change to 0-1
-#ds_array = img_array / 255
-
-#r = skimage.measure.block_reduce(ds_array[:, :, 0],
-#                                 (downsample, downsample),
-#                                 np.mean)
-#g = skimage.measure.block_reduce(ds_array[:, :, 1],
-#                                 (downsample, downsample),
-#                                 np.mean)
-#b = skimage.measure.block_reduce(ds_array[:, :, 2],
-#                                 (downsample, downsample),
-#                                 np.mean)
-#ds_array = np.stack((r, g, b), axis=-1)
-
-
-
-#fig, axes = plt.subplots(nrows=1, ncols=2)
-#ax = axes.ravel()
-
-#ax[0].imshow(img_array)
-#ax[0].set_title("Oryginal")
-#ax[0].set_xlabel(str(img_array.shape))
-
-#ax[1].imshow(ds_array)
-#ax[1].set_title("Downsampled by {0}x{0}".format(downsample))
-#ax[1].set_xlabel(str(ds_array.shape))
-
-
-#plt.tight_layout()
-
-
-##plt.imshow(img_array)
-
-
-#figManager = plt.get_current_fig_manager()
-#figManager.window.showMaximized()
-#plt.show()
-
-
-
-
-##############################################################################################################
-##############################################################################################################
-##############################################################################################################
-##############################################################################################################
-##############################################################################################################
-
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -133,7 +17,7 @@ PHOTO_DIR = "4K_3840x2160.jpg"
 # PHOTO_DIR = "00000_3840x2160.exr"
 
 
-def data_decimation(img, size, kernel_size, anchor):
+def data_decimation(img, size, kernel_size, anchor, dtype=np.float32):
     """Decimate a .exr file by given scale
     
         Args:
@@ -141,23 +25,23 @@ def data_decimation(img, size, kernel_size, anchor):
             size: shape of img
             kernel_size: determines a matrix to decimate (how rescale the image will be)
             anchor: position of pixel around of center of the image
-            # 00 01
-            # 10 11
+            # x,y x,y
+            # 0,0 0,1
+            # 1,0 1,1
     """
     
-    decimated_tensor = np.zeros((size[0] // kernel_size[0], size[1] // kernel_size[1], size[2]), dtype='uint8') 
+    decimated_tensor = np.zeros((size[0] // kernel_size[0], size[1] // kernel_size[1], size[2]), dtype=dtype) 
    
+    step_x = kernel_size[0]  #determines kernel step size rightwards x axis of image
+    step_y = kernel_size[1]  #determines kernel step size downwards y axis of image
 
-    step_x = kernel_size[0]
-    step_y = kernel_size[1]
-
-    offset_x = (kernel_size[0] // 2) - 1
-    offset_y = (kernel_size[1] // 2) - 1
+    offset_x = (kernel_size[0] // 2) - 1  #determines which id in x axis will be taken for given pixel position
+    offset_y = (kernel_size[1] // 2) - 1  #determines which id in y axis will be taken for given pixel position
     
     for i in range(decimated_tensor.shape[0]): 
         for j in range(decimated_tensor.shape[1]):
             for channel in range(decimated_tensor.shape[2]):  #Channel e.g: R G B, then we have a 3-channel image
-                decimated_tensor[i][j][channel] = img[i* step_x + offset_x + anchor[0]][j * step_y + offset_y + anchor[1]][channel]
+                decimated_tensor[i][j][channel] = img[i * step_x + offset_x + anchor[0]][j * step_y + offset_y + anchor[1]][channel]
 
     return decimated_tensor
 
